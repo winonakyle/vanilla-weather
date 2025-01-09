1a237cab2b29a56e330828f0a5dce839bd071e69
Winona Kyle <winonaksb@gmail.com>
2025-01-09 09:53:01 -0600

    added basic search engine

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-alert("hello!");
+function submitSearchForm(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let currentCityElement = document.querySelector("#current-city");
+  currentCityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", submitSearchForm);
